8b0fdd57e2067f06a7d7d7a248bbe057dc05f8c2
sialorama <sialorama@gmail.com>
2024-06-07 20:59:58 +0200

    Update du main

--- MarketLocal29/src/main/java/org/example/Main.java
+++ MarketLocal29/src/main/java/org/example/Main.java
@@ -10,22 +10,22 @@ public class Main {
         MarketController controller = new MarketController();
 
         // Create and add producers
-        Producer producer1 = new Producer(1, "John's Farm");
-        Producer producer2 = new Producer(2, "Green Valley");
+        Producer producer1 = new Producer(1, "Franc Halliday");
+        Producer producer2 = new Producer(2, "Marc Belleville");
 
         controller.addProducer(producer1);
         controller.addProducer(producer2);
 
         // Create and add products
-        Product product1 = new Product(1, "Apples", 0.5);
-        Product product2 = new Product(2, "Oranges", 0.7);
+        Product product1 = new Product(1, "Fraise de plougastel", 0.5);
+        Product product2 = new Product(2, "Pommes", 0.7);
 
         controller.addProduct(product1);
         controller.addProduct(product2);
 
         // Create and add orders
-        ClientOrder order1 = new ClientOrder(1, product1, 10);
-        ClientOrder order2 = new ClientOrder(2, product2, 5);
+        ClientOrder order1 = new ClientOrder(1, product1, 15);
+        ClientOrder order2 = new ClientOrder(2, product2, 8);
 
         controller.addOrder(order1);
         controller.addOrder(order2);
